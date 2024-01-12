4ffc4ba4e4c483ba72afa5a99dc26dbacafaa731
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-01-12 20:12:28 -0300

    :build: < Instalação mochawesome-report-generator, mochawesome-merge, mochawesom, moment para emitir relatórios dinamicos

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,23 @@
 const { defineConfig } = require("cypress");
+const moment = require('moment');
+
+const dateTime = moment().format('YYYYMMDD_HHmmss');
 
 module.exports = defineConfig({
   chromeWebSecurity:false,
   e2e: {
     baseUrl: 'https://elogroup.dev.br/metas/#/',
     video: true,
+    reporter: "mochawesome",
+    reporterOptions: {
+    reportDir: `cypress/reports/${dateTime}`,
+    quiet: true,
+    overwrite: false,
+    html: true,
+    json: false
+  },
+  screenshotsFolder: `cypress/reports/${dateTime}/evidências`,
+    
     
   },
   
